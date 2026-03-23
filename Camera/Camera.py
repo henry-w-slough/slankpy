d460f407a8d98cf15e936aaa93687ea5e0a487c4
henry-w-slough <henry.w.slough@gmail.com>
2026-03-22 22:31:50 -0400

    More camera work

--- Camera/Camera.py
+++ Camera/Camera.py
@@ -14,26 +14,19 @@ class Camera:
         self.viewport_rect = pygame.Rect(0, 0, pygame.display.get_surface().get_width(), pygame.display.get_surface().get_height())
 
         self.target:KinematicObject.KinematicObject
-        
-        self.last_x = 0
-        self.last_y = 0
-
-        self.offset_x = 0
-        self.offset_y = 0
-
-        self.screen_width = pygame.display.get_surface().get_width()
-        self.screen_height = pygame.display.get_surface().get_height()
 
 
-    def add_offset(self, layer:pygame.sprite.Group) -> None:
-        """Adds the offset of the Camera to the given object's position."""
-
+    def update(self) -> None:
+        self.viewport_rect.center = self.target.rect.center
 
 
     def get_visible(self, layer:pygame.sprite.Group) -> pygame.sprite.Group:
         """Used for object view culling. Goes through the given Group and removes sprites that aren't visible on the screen."""
+        visible = pygame.sprite.Group()
+
         for s in layer:
-            if not s.rect.colliderect(self.viewport_rect):
-                layer.remove(s)
-        return layer
+            if s.rect.colliderect(self.viewport_rect):
+                visible.add(s)
+
+        return visible
             
