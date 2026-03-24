06bbfe6e3dc40d707562e0a1d25fd33cbdea76bb
Henry Slough <henry.w.slough@gmail.com>
2026-03-23 21:34:18 -0400

    Added MapLoader map_to_group functionality

--- Map/MapLoader.py
+++ Map/MapLoader.py
@@ -1,5 +1,7 @@
 
 import json
+import pygame
+from ..Objects import KinematicObject
 
 
 def load_map(src:str) -> dict:
@@ -9,3 +11,23 @@ def load_map(src:str) -> dict:
     except Exception as e:
         print(f"ERROR: MapLoader: load_map: {e}")
         return {}
+    
+
+def map_to_group(map:dict) -> pygame.sprite.Group:
+
+    group = pygame.sprite.Group()
+
+    for layer in map["layers"]:
+        for index, tile_id in enumerate(layer["data"]):
+
+            x = (index % layer["width"]) * (map["tilewidth"])
+            y = (index // layer["height"]) * (map["tileheight"])
+
+            t = KinematicObject.KinematicObject(map["tilewidth"], map["tileheight"], group)
+            t.set_position(x, y)
+            t.sprite.add_sprites("tiles.png", "tiles", 2, 2)
+            t.set_sprite("tiles", tile_id-1)
+
+
+    return group
+
