6888bdb4cee5209f38a1964400ad2b28ffb73cef
Henry Slough <henry.w.slough@gmail.com>
2026-04-21 14:26:53 -0400

    Added vector handling function for Input class

--- Input/Input.py
+++ Input/Input.py
@@ -18,6 +18,17 @@ def is_key_pressed(*keys:int) -> bool:
     return False
 
 
+def get_input_vector(key1:int, key2:int) -> int:
+    """Gets the given input as a 2D vector, meaning it can be used for opposing input handling. Note that vector x and y values are returned in values of 1 or -1, meaning they are by default normalized."""
+    vector = 0
+    if pygame.key.get_pressed()[key1]:
+        vector -= 1
+    if pygame.key.get_pressed()[key2]:
+        vector += 1
+
+    return vector
+
+
 def get_mouse_position() -> tuple[int, int]:
     """Returns the position of the mouse cursor."""
     return pygame.mouse.get_pos()
