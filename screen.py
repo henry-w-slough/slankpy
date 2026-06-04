cb8eb41f27c1a258ebe2afe17f6089d1f8c638af
henry-w-slough <henry.w.slough@gmail.com>
2026-06-03 22:59:42 -0400

    Added layers to screen.Screen, also redid some docstrings and someprivitization conventions

--- screen.py
+++ screen.py
@@ -5,20 +5,20 @@ class Screen:
 
 
     def __init__(self, width: int, height: int, fps: float, *args, **kwargs) -> None:
-        
+        """Where all objects and layers are updated and drawn to the display window.
+
+        """
         #creating the window instance
         self._screen: pygame.Surface = pygame.display.set_mode((width, height), *args, **kwargs)
 
-        self._width = width
-        self._height = height
-
         #time-relations
         self._clock = pygame.time.Clock()
-        self._delta_time: float = 0
+        self._delta_time: float = 0.0
         self.fps = fps
 
         #screen layering
         self.background_color: tuple[int, int, int] = (0, 0, 0)
+        self._layers: dict[str, pygame.sprite.Group] = {}
 
 
     @property 
@@ -37,39 +37,63 @@ class Screen:
 
     @property
     def height(self) -> int:
-        return self._height
+        return self._screen.get_width()
 
 
     @property
     def width(self) -> int:
-        return self._width
+        return self._screen.get_height()
 
 
     @property
     def center(self) -> tuple[float, float]:
         """The position of the center of the screen."""
         return (self._screen.get_width() / 2, self._screen.get_height() / 2)
+    
+
+    @property
+    def layers(self) -> dict[str, pygame.sprite.Group]:
+        return self._layers
+    
+
+    def add_layer(self) -> None:
+        """Adds a layer to be updated and drawn every frame.
+
+        Note:
+            Layers are drawn in the order they fall in the dictionary. To take advantage of this,
+            add layers in the order of their perspective relative to the camera.
+        """
+    
+
+    def has_quit(self) -> bool:
+        """Whether the window has been exited.
+        
+        Note: 
+            Clears pygame.event.get() queue! If you need to access events
+            elsewhere, use pygame.event.peek() to avoid issues.
+        """
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return True
+        return False
 
 
     def update(self) -> None:
-        """Updates the display by flipping the buffer and redrawing all layers to the screen.
+        """Updates the screen by flipping the buffer. Updates and draws every layer in order.
         
         This is also where delta_time is updated.
         """
 
         self._screen.fill(self.background_color)
 
+        for layer in self._layers.values():
+            layer.update()
+            layer.draw(self._screen)
+
         pygame.display.flip()
-        self._delta_time = self._clock.tick_busy_loop(self.fps) / 1000
-
-
-    def has_quit(self) -> bool:
-        """Whether the window has been closed or ended."""
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return True
-        return False
+        #using 1000.0 to specify for float type
+        self._delta_time = self._clock.tick_busy_loop(self.fps) / 1000.0
     
     
 
