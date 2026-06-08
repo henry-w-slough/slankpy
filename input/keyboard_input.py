839b6bb92aa24f5d62ef87535ca8fa16465140f3
henry-w-slough <henry.w.slough@gmail.com>
2026-06-08 09:23:18 -0400

    Added keyboard input class with basic abstracted pygame.K_ int variables in seperate class

---
+++ input/keyboard_input.py
@@ -0,0 +1,10 @@
+import pygame
+
+
+def is_key_pressed(*keys: int) -> bool:
+    """Checks if the given keys are active in this frame."""
+    current_input = pygame.key.get_pressed()
+    for key in keys:
+        if current_input[key]:
+            return True
+    return False
