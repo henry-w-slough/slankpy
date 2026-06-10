bd7f0a3f7928af6fbc5e4e9ddbf2591e77bf1190
henry-w-slough <henry.w.slough@gmail.com>
2026-06-10 13:20:00 -0400

    Added GameObject and Sprite animation loading from spritesheet surface, derived from slankpy mk1

--- game_object/game_object.py
+++ game_object/game_object.py
@@ -19,20 +19,35 @@ class GameObject(pygame.sprite.Sprite):
 
 
         self._transform = Transform(width, height)
-        self.sprite = Sprite(width, height)
+        self._sprite = Sprite(width, height)
 
         #type hints are needed here for proper refs in methods
-        self.image: pygame.Surface = self.sprite.image
+        self.image: pygame.Surface = self._sprite.image
         self.rect: pygame.Rect = self.image.get_rect()
 
 
     def fill_color(self, color: tuple[int, int, int]) -> None:
-        """Fills the image of the GameObject with the given color.
+        """Fills the image of the GameObject with the given color (R, G, B).
         
         Useful for debugging or basic game-making where sprites aren't necessary.
         """
-        self.sprite.fill_color(color)
-        self.image = self.sprite.image 
+        self._sprite.fill_color(color)
+        self.image = self._sprite.image 
+
+
+    def add_animation(self, name: str, src: str, sprite_rows: int, sprite_columns: int) -> None:
+        """Adds a new animation dict of sprites derived from the given spritesheet image.
+        
+        Sprite sizes are calculated based on the number of sprite rows and columns provided."""
+        self._sprite.add_animation(name, src, sprite_rows, sprite_columns)
+
+
+    def set_sprite(self, animation_name: str, sprite_index: int) -> None:
+        """Sets the sprite to the given animation frame.
+        
+        Animations can be added with add_animation() of GameObject."""
+        self._sprite.set_sprite(animation_name, sprite_index)
+        self.image = self._sprite.image
 
 
     @property
@@ -87,10 +102,8 @@ class GameObject(pygame.sprite.Sprite):
     @rotation.setter
     def rotation(self, rotation: float) -> None:
         self._transform.rotation = rotation
-        self.sprite.image, self.rect = self._transform.get_rotated_surface(self.sprite.image_unmodified)
-        self.image = self.sprite.image
-
-        print(f"In GameObject: {id(self.image)}")
+        self._sprite.image, self.rect = self._transform.get_rotated_surface(self._sprite.image_unmodified)
+        self.image = self._sprite.image
 
 
 
--- game_object/sprite.py
+++ game_object/sprite.py
@@ -8,19 +8,82 @@ class Sprite():
         """Handles all animation and sprite-related operations to a GameObject.
         
         Holds a Sprite image, which is the image drawn on the GameObject image on the screen."""
+        
+        self.width = width
+        self.height = height
 
-        self.image = pygame.Surface((width, height))
+        self._image = pygame.Surface((width, height))
         self._image_unmodified = pygame.Surface((width, height))
-    
 
+        self.animations: dict[str, dict[int, pygame.Surface]] = {}
+    
+    
     @property
     def image_unmodified(self) -> pygame.Surface:
-        """The image of the sprite without transformations such as rotation."""
+        """The untransformed image of the sprite, unaffected by operations like rotation."""
         return self._image_unmodified
 
 
     def fill_color(self, color:tuple[int, int, int]) -> None:
         """Fills the image with the given color."""
-        self.image.fill(color)
+        self._image.fill(color)
         self._image_unmodified.fill(color)
+
+
+    @property
+    def image(self) -> pygame.Surface:
+        return self._image
+    
+
+    @image.setter
+    def image(self, image:pygame.Surface) -> None:
+        self._image = image
+    
+
+    def set_sprite(self, animation_name: str, sprite_index: int) -> None:
+        self.image = self.animations[animation_name][sprite_index]
+
+
+    def add_animation(self, name: str, src: str, sprite_rows: int, sprite_columns: int) -> None:
+        """Adds an animation from the given image source 
+        
+        The given image is cut into seperate sprites based on the amount of sprite rows and columns provided.
+        
+        Sprites are added by their placement from left to right in the spritesheet, starting at 0."""
+
+        spritesheet = pygame.image.load(src).convert_alpha()
+
+        #empty animation dict
+        self.animations[name] = {}
+
+        #calculating size of sprite from the given num of sprites and the full size
+        sprite_width = spritesheet.get_width() / sprite_rows
+        sprite_height = spritesheet.get_height() / sprite_columns
+
+        sprite_num = 0
+
+        column = 0
+        row = 0
+        
+        #the surface rect of each sprite
+        sprite_rect = pygame.Rect(0, 0, sprite_width, sprite_height)
+
+        while column != sprite_columns:
+            
+            #prevents making a new rect every frame
+            sprite_rect.width = row*sprite_width
+            sprite_rect.height = column*sprite_height
+
+            #section of image for new sprite
+            new_sprite = spritesheet.subsurface(sprite_rect)
+
+            self.animations[name][sprite_num] = new_sprite
+
+            sprite_num += 1
+
+            #iterating over from left to right of the image
+            row += 1
+            if row == sprite_rows:
+                column += 1
+                row = 0
     
