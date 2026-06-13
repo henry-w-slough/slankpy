128ecd30989c3b6123045bcdab338aec11a739b0
henry-w-slough <henry.w.slough@gmail.com>
2026-06-12 20:08:18 -0400

    Fixed __init__ improper import for input

--- __init__.py
+++ __init__.py
@@ -3,5 +3,5 @@
 from .game_object import GameObject
 from .screen import Screen
 from .input import keys
-from .input import input as Input
-from .input import keys as keys
+from .input import input
+from .input import keys
